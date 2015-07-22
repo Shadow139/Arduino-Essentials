880cb82ba8cd32195690f78eca268d2c1f563ae8
Shadow139 <cloud99_9@hotmail.com>
2015-07-22 17:24:59 +0200

    temperature and humidity sensor sketch and code

---
+++ temperature and humidiy/temperature_and_humidity/temperature_and_humidity.ino
@@ -0,0 +1,29 @@
+#include "DHT.h"
+
+#define DHTPIN 2
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN,DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Testing DHT Sensor");
+  dht.begin();
+
+}
+
+void loop() {
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+  if(isnan(temperature) || isnan(humidity)){
+    Serial.println("DHT could not be read!");
+  }else{
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print(" %\t");
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.print(" *C");
+  }
+
+}
